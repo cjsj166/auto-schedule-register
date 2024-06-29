aef11f981461abf12b5c42170e712461b3baaf73
cjsj166 <dohyung200@gmail.com>
2024-06-30 01:43:09 +0900

    First alert deleted

--- content.js
+++ content.js
@@ -1,8 +1,6 @@
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'eventCreated') {
-    alert(`Calendar event created successfully!`);
-
     if (window.confirm('Calendar event created successfully! Click "ok" to check the created event.')) {
       chrome.runtime.sendMessage({ action: 'createCalendarTab', link: message.link });
     };
