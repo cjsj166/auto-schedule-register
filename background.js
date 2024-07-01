24c38ab05a8fb9d66d4d68a148cb1ff631bf2786
cjsj166 <dohyung200@gmail.com>
2024-07-02 00:43:35 +0900

    async error resolved

--- background.js
+++ background.js
@@ -1,19 +1,5 @@
-url = null;
-apiKey = null;
-
-// 일정 데이터
-const eventSchedule = {
-  summary: '',
-  description: '',
-  start: {
-    dateTime: '',
-    timeZone: 'Asia/Tokyo'
-  },
-  end: {
-    dateTime: '',
-    timeZone: 'Asia/Tokyo'
-  }
-};
+// url = null;
+// apiKey = null;
 
 // global variable
 currentTabId = null;
@@ -21,15 +7,9 @@ popupWindowId = null
 
 // Make "Fetch GPT-3 Response" button in right click menus
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.get(['apiUrl', 'apiKey'], function (items) {
-
-    url = 'https://api.openai.com/v1/chat/completions'
-    apiKey = items.apiKey;
-  });
-
   chrome.contextMenus.create({
     id: "fetch-gpt3-response",
-    title: "Fetch GPT-3 Response",
+    title: "Register Schedule",
     contexts: ["selection"]
   });
 
@@ -43,7 +23,6 @@ chrome.runtime.onInstalled.addListener(() => {
         // console.log("Result: ", result);
         if (result.eventInfo) {
           // console.log(result.eventInfo);
-
           createCalendarEvent(result.eventInfo).then(response => {
             sendResponse(response);
           });
@@ -57,7 +36,6 @@ chrome.runtime.onInstalled.addListener(() => {
   // Make a listener to close the window after event is registered in Google Calendar
   chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.action === 'closePopup') {
-
       chrome.storage.local.get(['windowId'], (result) => {
         if (result.windowId) {
           chrome.windows.remove(result.windowId, function () {
@@ -73,7 +51,6 @@ chrome.runtime.onInstalled.addListener(() => {
       chrome.tabs.create({ url: message.link });
     }
   });
-
 });
 
 
@@ -91,7 +68,7 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
     }
 
     // A window for confirming event details
-    chrome.windows.create({
+    await chrome.windows.create({
       url: chrome.runtime.getURL("popup.html"),
       type: "popup",
       width: 400,
@@ -101,14 +78,22 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
     });
 
     // Fetch organized schedule info using GPT-3
-    const eventSchedule = await fetchScheduleByGPT3Response(selectedText);
+    console.log('call fetchScheduleByGPT3Response')
+    eventSchedule = null;
+    await chrome.storage.sync.get(['apiUrl', 'apiKey'], async function (items) {
+      eventSchedule = await fetchScheduleByGPT3Response(selectedText, items.apiKey);
 
-    // Save the event info to local storage so that it can be accessed in popup.html
-    chrome.storage.local.set({ 'eventInfo': eventSchedule }, function () {
-      // console.log('Value is set to ' + eventSchedule);
-      chrome.runtime.sendMessage({ action: 'fillValue' }, response => {
+      console.log('send eventSchedule to popup:', eventSchedule);
+
+      // Save the event info to local storage so that it can be accessed in popup.html
+      chrome.storage.local.set({ 'eventInfo': eventSchedule }, function () {
+        // console.log('Value is set to ' + eventSchedule);
+        chrome.runtime.sendMessage({ action: 'fillValue' }, response => {
+        });
       });
+
     });
+
 
     // console.log(eventSchedule)
 
@@ -116,66 +101,8 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
 });
 
 
-// //
-// "Information about arranged schedule will be provided.\n\
-// Do your best to describe the schedule as concisely as possible in json format.\n\
-// Required json format is as follow\nY:year M:month D:day h:hour m:minute s:second X:unknown\n\
-// You can use X instead of any other character if you cannot determine specific year, month, date ex)XXXX-02-13T15:20:00\n\
-// { \"starting_date_time\": \"YYYY-MM-DDThh:mm:ss\", \"end_date_time\": \"YYYY-MM-DDThh:mm:ss\", \"title\": \"Title of the event\", \"description\": \"description of the meeting, meeting room url, address of the place, etc.\" }"
-// //
-
-// "Information about arranged schedule will be provided.\n\
-// Do your best to describe the schedule as concisely as possible in json format.\n\
-// Required json format is as follow.\n\
-// { \"starting_date_time\": \"YYYY-MM-DDThh:mm:ss\", \"end_date_time\": \"YYYY-MM-DDThh:mm:ss\", \"title\": \"Title of the event\", \"description\": \"description of the meeting, meeting room url, address of the place, etc.\" }\n\
-// If you only know relative time duration, the json format should be as follow.\n\
-// { \"starting_date_time\": \"PnYnMnDTnHnMnS\", \"end_date_time\": \"PnYnMnDTnHnMnS\", \"title\": \"Title of the event\", \"description\": \"description of the meeting, meeting room url, address of the place, etc.\" }"
-// //
-
-// "Information about arranged schedule will be provided.\n\
-// Do your best to describe the schedule as concisely as possible in json format.\n\
-// Required json format is as follow.\n\
-// { \"starting_date_time\": \"PnYnMnDTnHnMnS\", \"end_date_time\": \"PnYnMnDTnHnMnS\", \"title\": \"Title of the event\", \"description\": \"description of the meeting, meeting room url, address of the place, etc.\" }\
-// The ISO 8601 duration format \"PnYnMnDTnHnMnS\" represents a time interval from now:\n\
-// P: Period start.\n\
-// nY: Number of years.\n\
-// nM: Number of months.\n\
-// nW: Number of weeks.\n\
-// nD: Number of days.\n\
-// T: Time start (separates date and time).\n\
-// nH: Number of hours.\n\
-// nM: Number of minutes.\n\
-// nS: Number of seconds.\n\
-// If you know the exact time, the json format should be as follow.\n\
-// { \"starting_date_time\": \"YYYY-MM-DDThh:mm:ss\", \"end_date_time\": \"YYYY-MM-DDThh:mm:ss\", \"title\": \"Title of the event\", \"description\": \"description of the meeting, meeting room url, address of the place, etc.\" }"
-// //
-
-// "Information about arranged schedule will be provided.\n\
-// Do your best to describe the schedule as concisely as possible in json format.\n\
-// Required json format is as follow.\n\
-// { \"starting_date_time\": \"PnYnMnDTnHnMnS\", \"end_date_time\": \"PnYnMnDTnHnMnS\", \"title\": \"Title of the event\", \"description\": \"description of the meeting, meeting room url, address of the place, etc.\" }\
-// The ISO 8601 duration format \"PnYnMnDTnHnMnS\" represents a time interval from now:\n\
-// P: Period start.\n\
-// nY: Number of years.\n\
-// nM: Number of months.\n\
-// nW: Number of weeks.\n\
-// nD: Number of days.\n\
-// T: Time start (separates date and time).\n\
-// nH: Number of hours.\n\
-// nM: Number of minutes.\n\
-// nS: Number of seconds.\n\
-// If you know the exact time, the json format should be as follow.\n\
-// { \"starting_date_time\": \"YYYY-MM-DDThh:mm:ss\", \"end_date_time\": \"YYYY-MM-DDThh:mm:ss\", \"title\": \"Title of the event\", \"description\": \"description of the meeting, meeting room url, address of the place, etc.\" }"
-// //
-
-//   `Information about arranged schedule will be provided.\n\
-// Do your best to describe the schedule as concisely as possible in json format.\n\
-// Consider the date now : ${isoString}.\n\" + "Required json format is as follow\n\
-// { \"starting_date_time\": \"YYYY-MM-DDThh:mm:ss\", \"end_date_time\": \"YYYY-MM-DDThh:mm:ss\", \"title\": \"Title of the event\", \"description\": \"description of the meeting, meeting room url, address of the place, etc.\" }`
-
-
 // Process information about scheduling into json file using gpt3 api call
-async function fetchScheduleByGPT3Response(inputText) {
+async function fetchScheduleByGPT3Response(inputText, apiKey) {
   const now = new Date();
   const isoString = now.toISOString();
   prompt_text = `Information about arranged schedule will be provided.\n\
@@ -184,10 +111,15 @@ Consider the date now : ${isoString}.\n\" + "Required json format is as follow.\
 { \"starting_date_time\": \"YYYY-MM-DDThh:mm:ss\", \"end_date_time\": \"YYYY-MM-DDThh:mm:ss\", \"title\": \"Title of the event\", \"description\": \"description of the meeting, meeting room url, address of the place, etc.\" \
  For title and description, you need to answer in the same language in which you were asked.}`
 
+  const url = 'https://api.openai.com/v1/chat/completions';
+
+  console.log('key : ', apiKey);
+
   const headers = {
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${apiKey}`
   };
+
   const body = JSON.stringify({
     model: "gpt-3.5-turbo-1106",
     response_format: { "type": "json_object" },
@@ -197,9 +129,24 @@ Consider the date now : ${isoString}.\n\" + "Required json format is as follow.\
     ]
   });
 
-  console.log(prompt_text)
-
+  console.log('headers:')
+  console.log(headers)
+  console.log('body:')
+  console.log(body)
   console.log('fetchGPT3Response called with input:', inputText);
+
+  const eventSchedule = {
+    summary: '',
+    description: '',
+    start: {
+      dateTime: '',
+      timeZone: 'Asia/Tokyo'
+    },
+    end: {
+      dateTime: '',
+      timeZone: 'Asia/Tokyo'
+    }
+  };
 
   try {
     const response = await fetch(url, {
@@ -214,7 +161,6 @@ Consider the date now : ${isoString}.\n\" + "Required json format is as follow.\
 
     console.log('schedule info in GPT-3 response:', scheduleName);
     schedule = JSON.parse(scheduleName);
-    // console.log(schedule);
 
     eventSchedule.description = schedule.description;
     eventSchedule.summary = schedule.title;
@@ -223,16 +169,14 @@ Consider the date now : ${isoString}.\n\" + "Required json format is as follow.\
     eventSchedule.start.timeZone = 'Asia/Tokyo';
     eventSchedule.end.timeZone = 'Asia/Tokyo';
 
-    // console.log(eventSchedule);
-
     return eventSchedule;
+
   } catch (error) {
     console.error('Error fetching GPT-3 response:', error);
     throw error;
   }
+
 }
-
-
 
 // Get token by OAuth2 authentication 
 function getAccessToken(interactive) {
--- content.js
+++
@@ -1,9 +0,0 @@
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'eventCreated') {
-    if (window.confirm('Calendar event created successfully! Click "ok" to check the created event.')) {
-      chrome.runtime.sendMessage({ action: 'createCalendarTab', link: message.link });
-    };
-
-  }
-});
